41486448ea28d7d26222518f3acfc8645f813ce3
Nokimochi1 <75022556+Nokimochi1@users.noreply.github.com>
2021-06-13 11:37:48 +0200

    Add files via upload

--- MajorFactory.sol
+++ MajorFactory.sol
@@ -1,51 +1,17 @@
-pragma solidity >=0.5.16;
+pragma solidity >=0.5.0;
 
-import './interfaces/IMajorFactory.sol';
-import './MajorPair.sol';
-
-contract MajorFactory is IMajorFactory {
-    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(PancakePair).creationCode));
-
-    address public feeTo;
-    address public feeToSetter;
-
-    mapping(address => mapping(address => address)) public getPair;
-    address[] public allPairs;
-
+interface MajorFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
-    constructor(address _feeToSetter) public {
-        feeToSetter = _feeToSetter;
-    }
+    function feeTo() external view returns (address);
+    function feeToSetter() external view returns (address);
 
-    function allPairsLength() external view returns (uint) {
-        return allPairs.length;
-    }
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function allPairs(uint) external view returns (address pair);
+    function allPairsLength() external view returns (uint);
 
-    function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'Pancake: IDENTICAL_ADDRESSES');
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'Pancake: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'Pancake: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(PancakePair).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
-        assembly {
-            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
-        }
-        IPancakePair(pair).initialize(token0, token1);
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair; // populate mapping in the reverse direction
-        allPairs.push(pair);
-        emit PairCreated(token0, token1, pair, allPairs.length);
-    }
+    function createPair(address tokenA, address tokenB) external returns (address pair);
 
-    function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'Major Finance: FORBIDDEN');
-        feeTo = _feeTo;
-    }
-
-    function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'Major Finance: FORBIDDEN');
-        feeToSetter = _feeToSetter;
-    }
+    function setFeeTo(address) external;
+    function setFeeToSetter(address) external;
 }
